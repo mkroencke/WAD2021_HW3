180e147e1a14cd981aebf13a322feae892e475f6
mkroencke <m.kroencke@outlook.de>
2021-11-21 20:56:13 +0200

    Remove unused code

--- src/router/index.js
+++ src/router/index.js
@@ -4,20 +4,6 @@ import Login from "../views/Login.vue";
 import Main from "../views/Main.vue";
 
 const routes = [
-  /*{
-    path: "/",
-    name: "Home",
-    component: Home,
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ //"../views/About.vue"),
-  //},
   {
     path: "/",
     name: "main",
